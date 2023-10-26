77deeaac91cae6d64d23b78e297392e44d7ef7ef
unknown <helenvcolon@gmail.com>
2023-10-26 05:05:01 -0400

    edit html removed starter code comments, edit css,defined variables and edited functions current date and time block.

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,33 @@
 $(function () {
+  // Defined Variables
+  // Variables to store the current hour and dates using Day.js
+  var currentHour = dayjs().hour();
+  var currentDate = dayjs().format("dddd, MMMM D");
+
   // Function to display current date in the header
   function displayCurrentDate() {
-    var currentDate = dayjs().format("dddd, MMMM D");
     $("#currentDay").text(currentDate);
   }
 
   // Function to update time-block colors based on current time
   function updateTimeBlockColors() {
-    var currentHour = dayjs().hour();
-
     $(".time-block").each(function() {
-      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+      var blockHour = parseInt(this.id.split("-") [1]);
 
+      // Remove existing classes
+      $(this).removeClass("past present future");
+      
+      // Add appropriate class based on comparison with currentHour
       if (blockHour < currentHour) {
-        $(this).removeClass("present future").addClass("past");
+        $(this).addClass("past");
       } else if (blockHour === currentHour) {
-        $(this).removeClass("past future").addClass("present");
-       } else {
-        $ (this).removeClass("past present").addClass("future");
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
       }
-  });
-}
+    });
+  }
+
 
 
   
